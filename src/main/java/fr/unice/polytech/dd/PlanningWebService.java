7162e0d6eb54ae109545f29d492054682b4055a4
PaulK <merveille.koffi@etu.unice.fr>
2020-05-03 19:50:57 +0200

    #63 add Exceptions

--- src/main/java/fr/unice/polytech/dd/PlanningWebService.java
+++ src/main/java/fr/unice/polytech/dd/PlanningWebService.java
@@ -1,6 +1,7 @@
 package fr.unice.polytech.dd;
 
 import javax.jws.WebMethod;
+import javax.jws.WebParam;
 import javax.jws.WebResult;
 import javax.jws.WebService;
 
@@ -9,5 +10,20 @@ public interface PlanningWebService {
 
     @WebMethod
     @WebResult(name="register_delivery")
-    void register_delivery(String name_client, String name_provider);
+    String register_delivery(@WebParam(name="name_client") String nameClient,
+                             @WebParam(name="number_secret") String numberSecret,
+                             @WebParam(name="delivery_date") String deliveryDate,
+                             @WebParam(name="hour_delivery") String hourDelivery) throws Exception;
+
+    @WebMethod
+    @WebResult(name="repogramming_delivery")
+    String repogramming_delivery(@WebParam(name="old_date") String oldDate,
+                             @WebParam(name="old_hour") String oldHour,
+                             @WebParam(name="delivery_date") String deliveryDate,
+                             @WebParam(name="hour_delivery") String hourDelivery) throws Exception;
+
+    @WebMethod
+    @WebResult(name = "valid_slot")
+    Boolean validSlot(@WebParam(name="delivery_date") String deliveryDate,
+                      @WebParam(name="hour_delivery") String hourDelivery) throws Exception;
 }
--- src/main/java/fr/unice/polytech/dd/PlanningWebServiceImpl.java
+++ src/main/java/fr/unice/polytech/dd/PlanningWebServiceImpl.java
@@ -1,22 +1,38 @@
 package fr.unice.polytech.dd;
 
-/*
+
+import fr.unice.polytech.isa.dd.AvailableSlotTime;
+import fr.unice.polytech.isa.dd.DeliveryRegistration;
+
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.jws.WebService;
 
 @WebService(targetNamespace = "http://www.polytech.unice.fr/si/4a/isa/dd/planningService")
-
 @Stateless(name = "PlanningWS")
 public class PlanningWebServiceImpl implements PlanningWebService {
 
-    @EJB(name="planning-stateless") private DeliveryRegistration deliveryRegistration;
+    @EJB(name = "planning-stateless")
+    private DeliveryRegistration deliveryRegistration;
+    @EJB(name = "planning-stateless")
+    private AvailableSlotTime availableSlotTime;
+
 
     @Override
-    public void register_delivery(String name_client, String name_provider) {
+    public String register_delivery(String nameClient, String numberSecret, String deliveryDate, String hourDelivery) throws Exception {
+        System.out.println("Enregistrement d'une nouvelle livraison");
+        return deliveryRegistration.register_delivery(nameClient,numberSecret,deliveryDate,hourDelivery);
+    }
 
-       // deliveryRegistration.register_delivery(name_client,name_provider);
+    @Override
+    public Boolean validSlot(String deliveryDate, String hourDelivery) throws Exception {
+        System.out.println("Validation d'un horaire");
+        return  availableSlotTime.valid_slot_time(deliveryDate,hourDelivery);
+    }
 
+    @Override
+    public  String repogramming_delivery(String oldDate,String oldHour, String deliveryDate, String hourDelivery) throws Exception {
+        System.out.println("Reprogrammation d'un horaire");
+        return deliveryRegistration.repogramming_delivery(oldDate,oldHour,deliveryDate,hourDelivery);
     }
 }
-*/
