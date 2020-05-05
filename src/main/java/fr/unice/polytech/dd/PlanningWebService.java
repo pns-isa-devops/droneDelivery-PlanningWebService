829d52cf7b39ade36bbbf81b93636614aef06d31
PaulK <merveille.koffi@etu.unice.fr>
2020-05-05 21:59:01 +0200

    #14 add exceptions to delivery service implementation

--- src/main/java/fr/unice/polytech/dd/PlanningWebService.java
+++ src/main/java/fr/unice/polytech/dd/PlanningWebService.java
@@ -1,5 +1,10 @@
 package fr.unice.polytech.dd;
 
+import fr.unice.polytech.isa.dd.exceptions.PackageAlreadyTookException;
+import fr.unice.polytech.isa.dd.exceptions.UnvailableSlotTimeException;
+import fr.unice.polytech.isa.dd.exceptions.UnknownCustomerException;
+import fr.unice.polytech.isa.dd.exceptions.UnknownPackageException;
+
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebResult;
@@ -13,17 +18,17 @@ public interface PlanningWebService {
     String register_delivery(@WebParam(name="name_client") String nameClient,
                              @WebParam(name="number_secret") String numberSecret,
                              @WebParam(name="delivery_date") String deliveryDate,
-                             @WebParam(name="hour_delivery") String hourDelivery) throws Exception;
+                             @WebParam(name="hour_delivery") String hourDelivery) throws PackageAlreadyTookException, UnvailableSlotTimeException, UnknownCustomerException, UnknownPackageException, java.text.ParseException;
 
     @WebMethod
     @WebResult(name="repogramming_delivery")
     String repogramming_delivery(@WebParam(name="old_date") String oldDate,
                              @WebParam(name="old_hour") String oldHour,
                              @WebParam(name="delivery_date") String deliveryDate,
-                             @WebParam(name="hour_delivery") String hourDelivery) throws Exception;
+                             @WebParam(name="hour_delivery") String hourDelivery) throws UnvailableSlotTimeException, java.text.ParseException;
 
     @WebMethod
     @WebResult(name = "valid_slot")
     Boolean validSlot(@WebParam(name="delivery_date") String deliveryDate,
-                      @WebParam(name="hour_delivery") String hourDelivery) throws Exception;
+                      @WebParam(name="hour_delivery") String hourDelivery) throws java.text.ParseException;
 }
--- src/main/java/fr/unice/polytech/dd/PlanningWebServiceImpl.java
+++ src/main/java/fr/unice/polytech/dd/PlanningWebServiceImpl.java
@@ -3,6 +3,10 @@ package fr.unice.polytech.dd;
 
 import fr.unice.polytech.isa.dd.AvailableSlotTime;
 import fr.unice.polytech.isa.dd.DeliveryRegistration;
+import fr.unice.polytech.isa.dd.exceptions.PackageAlreadyTookException;
+import fr.unice.polytech.isa.dd.exceptions.UnvailableSlotTimeException;
+import fr.unice.polytech.isa.dd.exceptions.UnknownCustomerException;
+import fr.unice.polytech.isa.dd.exceptions.UnknownPackageException;
 
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
@@ -19,20 +23,20 @@ public class PlanningWebServiceImpl implements PlanningWebService {
 
 
     @Override
-    public String register_delivery(String nameClient, String numberSecret, String deliveryDate, String hourDelivery) throws Exception {
+    public String register_delivery(String nameClient, String numberSecret, String deliveryDate, String hourDelivery) throws PackageAlreadyTookException, UnvailableSlotTimeException, UnknownCustomerException, UnknownPackageException, java.text.ParseException {
         System.out.println("Enregistrement d'une nouvelle livraison");
-        return deliveryRegistration.register_delivery(nameClient,numberSecret,deliveryDate,hourDelivery);
+        return deliveryRegistration.register_delivery(nameClient, numberSecret, deliveryDate, hourDelivery);
     }
 
     @Override
-    public Boolean validSlot(String deliveryDate, String hourDelivery) throws Exception {
+    public Boolean validSlot(String deliveryDate, String hourDelivery) throws java.text.ParseException {
         System.out.println("Validation d'un horaire");
-        return  availableSlotTime.valid_slot_time(deliveryDate,hourDelivery);
+        return availableSlotTime.valid_slot_time(deliveryDate, hourDelivery);
     }
 
     @Override
-    public  String repogramming_delivery(String oldDate,String oldHour, String deliveryDate, String hourDelivery) throws Exception {
+    public String repogramming_delivery(String oldDate, String oldHour, String deliveryDate, String hourDelivery) throws UnvailableSlotTimeException, java.text.ParseException {
         System.out.println("Reprogrammation d'un horaire");
-        return deliveryRegistration.repogramming_delivery(oldDate,oldHour,deliveryDate,hourDelivery);
+        return deliveryRegistration.repogramming_delivery(oldDate, oldHour, deliveryDate, hourDelivery);
     }
 }
