801f2f6f9dfb364dce5707305bd7f68bcb14bbd0
PaulmarieDjekinnou <pmdjek@gmail.com>
2020-05-07 03:47:30 +0200

    add interceptors to verify date

--- src/main/java/fr/unice/polytech/dd/PlanningWebService.java
+++ src/main/java/fr/unice/polytech/dd/PlanningWebService.java
@@ -4,7 +4,11 @@ import fr.unice.polytech.isa.dd.exceptions.PackageAlreadyTookException;
 import fr.unice.polytech.isa.dd.exceptions.UnvailableSlotTimeException;
 import fr.unice.polytech.isa.dd.exceptions.UnknownCustomerException;
 import fr.unice.polytech.isa.dd.exceptions.UnknownPackageException;
+import fr.unice.polytech.isa.dd.interceptors.DateVerifierForRegister;
+import fr.unice.polytech.isa.dd.interceptors.DateVerifierForReschedule;
+import fr.unice.polytech.isa.dd.interceptors.DateVerifierForValidSlot;
 
+import javax.interceptor.Interceptors;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebResult;
@@ -15,6 +19,7 @@ public interface PlanningWebService {
 
     @WebMethod
     @WebResult(name="register_delivery")
+    @Interceptors({DateVerifierForRegister.class})
     String register_delivery(@WebParam(name="name_client") String nameClient,
                              @WebParam(name="number_secret") String numberSecret,
                              @WebParam(name="delivery_date") String deliveryDate,
@@ -22,6 +27,7 @@ public interface PlanningWebService {
 
     @WebMethod
     @WebResult(name="repogramming_delivery")
+    @Interceptors({DateVerifierForReschedule.class})
     String repogramming_delivery(@WebParam(name="old_date") String oldDate,
                              @WebParam(name="old_hour") String oldHour,
                              @WebParam(name="delivery_date") String deliveryDate,
@@ -29,6 +35,7 @@ public interface PlanningWebService {
 
     @WebMethod
     @WebResult(name = "valid_slot")
+    @Interceptors({DateVerifierForValidSlot.class})
     Boolean validSlot(@WebParam(name="delivery_date") String deliveryDate,
                       @WebParam(name="hour_delivery") String hourDelivery) throws java.text.ParseException;
 }
