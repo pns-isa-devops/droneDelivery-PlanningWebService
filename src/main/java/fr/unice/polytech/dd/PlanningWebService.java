96b2ea7e38322f28212b944bbc0574d9c9d3b460
PaulmarieDjekinnou <pmdjek@gmail.com>
2020-05-07 05:04:59 +0200

    add hours interceptors

--- src/main/java/fr/unice/polytech/dd/PlanningWebService.java
+++ src/main/java/fr/unice/polytech/dd/PlanningWebService.java
@@ -4,9 +4,12 @@ import fr.unice.polytech.isa.dd.exceptions.PackageAlreadyTookException;
 import fr.unice.polytech.isa.dd.exceptions.UnvailableSlotTimeException;
 import fr.unice.polytech.isa.dd.exceptions.UnknownCustomerException;
 import fr.unice.polytech.isa.dd.exceptions.UnknownPackageException;
-import fr.unice.polytech.isa.dd.interceptors.DateVerifierForRegister;
-import fr.unice.polytech.isa.dd.interceptors.DateVerifierForReschedule;
-import fr.unice.polytech.isa.dd.interceptors.DateVerifierForValidSlot;
+import fr.unice.polytech.isa.dd.interceptors.dateinterceptors.DateVerifierForRegister;
+import fr.unice.polytech.isa.dd.interceptors.dateinterceptors.DateVerifierForReschedule;
+import fr.unice.polytech.isa.dd.interceptors.dateinterceptors.DateVerifierForValidSlot;
+import fr.unice.polytech.isa.dd.interceptors.hourinterceptors.HourVerifierForRegister;
+import fr.unice.polytech.isa.dd.interceptors.hourinterceptors.HourVerifierForValidSlot;
+import fr.unice.polytech.isa.dd.interceptors.hourinterceptors.HourVerifierReschedule;
 
 import javax.interceptor.Interceptors;
 import javax.jws.WebMethod;
@@ -19,7 +22,7 @@ public interface PlanningWebService {
 
     @WebMethod
     @WebResult(name="register_delivery")
-    @Interceptors({DateVerifierForRegister.class})
+    @Interceptors({DateVerifierForRegister.class, HourVerifierForRegister.class})
     String register_delivery(@WebParam(name="name_client") String nameClient,
                              @WebParam(name="number_secret") String numberSecret,
                              @WebParam(name="delivery_date") String deliveryDate,
@@ -27,7 +30,7 @@ public interface PlanningWebService {
 
     @WebMethod
     @WebResult(name="repogramming_delivery")
-    @Interceptors({DateVerifierForReschedule.class})
+    @Interceptors({DateVerifierForReschedule.class, HourVerifierReschedule.class})
     String repogramming_delivery(@WebParam(name="old_date") String oldDate,
                              @WebParam(name="old_hour") String oldHour,
                              @WebParam(name="delivery_date") String deliveryDate,
@@ -35,7 +38,7 @@ public interface PlanningWebService {
 
     @WebMethod
     @WebResult(name = "valid_slot")
-    @Interceptors({DateVerifierForValidSlot.class})
+    @Interceptors({DateVerifierForValidSlot.class, HourVerifierForValidSlot.class})
     Boolean validSlot(@WebParam(name="delivery_date") String deliveryDate,
                       @WebParam(name="hour_delivery") String hourDelivery) throws java.text.ParseException;
 }
